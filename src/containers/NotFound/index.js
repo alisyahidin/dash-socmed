1ffef55d0e0c27b9f6d8796f9aa05b96577d002f
Ali Syahidin <ali.idinz@gmail.com>
2019-02-23 05:34:17 +0700

    update snapshots

--- src/containers/NotFound/index.js
+++ src/containers/NotFound/index.js
@@ -4,7 +4,7 @@ import './style.scss'
 class NotFound extends Component {
   render() {
     return (
-      <div className="not-found-page">
+      <div className="not-found-page d-flex align-items-center justify-content-center">
         <h1>Not Found</h1>
       </div>
     )
